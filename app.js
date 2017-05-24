ea9a1a1699af36abf5f7cae41f2b48343ba19bde
Alvaro Bernar Silos <alvarobernarsilos@MacBook-Pro-de-Alvaro-2.local>
2017-05-24 10:38:38 +0200

    development created

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var api = require('./routes/api/index');
+var app = express();
+var  authRoutes = require('./routes/api/auth/index');
+const session    = require('express-session');
+const passport   = require('passport');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/petkeepers');
+
+const passportSetup = require('./config/passport');
+passportSetup(passport);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.render('./public/index.html');
+});
+
+app.use('/users', users);
+app.use('/api', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/api/users/index.js
@@ -0,0 +1,59 @@
+const express    = require('express');
+const User       = require('../../../models/User');
+const usersRoutes = express.Router();
+
+usersRoutes.post('/:id/location', (req, res, next) => {
+  let location = {
+    type: 'Point',
+    coordinates: ['40.416775', 	'-3.703790']
+  };
+
+  User.findOneAndUpdate(
+     { "facebookID" : req.params.id },
+     { $set: {
+       "location": location}
+     }, (err, user) => {
+       if (err) {
+         console.log(err);
+       } else if (user) {
+         console.log("user: "+user);
+       }
+     });
+});
+
+usersRoutes.post('/:id/keeper', (req, res, next) => {
+  let keeper;
+  let keeperPromise = new Promise((resolve, reject) => {
+    if (!req.body.keeper) {
+      User.findOne({"facebookID":req.params.id},
+      { keeper: 1}, (err, user) => {
+        if (user.keeper === true) keeper = false;
+        else if (user.keeper === false) keeper = true;
+        resolve(keeper);
+      });
+    } else if (req.body.keeper) {
+      keeper = req.body.keeper;
+      resolve(keeper);
+    }
+  });
+  keeperPromise.then((keeper) => {
+    User.findOneAndUpdate(
+      {"facebookID":req.params.id},
+      { $set: {
+        "keeper": keeper }
+      }, (err, user) => {
+        if (err) {
+          console.log(err);
+        } else if (user) {
+          console.log("user: "+user);
+        }
+      }
+    );
+  });
+
+
+
+
+});
+
+module.exports = usersRoutes;
